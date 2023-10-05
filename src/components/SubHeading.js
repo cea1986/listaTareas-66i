b461cabd0ec5df219d2dc2668ae2eab5386afb76
ito <carloseduardoarias86@gmail.com>
2023-10-05 01:28:31 -0300

    change readmi

---
+++ src/components/SubHeading.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class SubHeading extends Component {
+  render() {
+    return (
+      <div>Lista de tareas usando React </div>
+    )
+  }
+}
